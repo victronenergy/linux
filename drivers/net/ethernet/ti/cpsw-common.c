9f8f2c2065457c08db0b29fdad98f5b74f3a911a
Jeroen Hofstee <jhofstee@victronenergy.com>
2016-10-20 21:29:26 +0200

    net: cpsw: fix obtaining mac address for am3517
    Commit b6745f6e4e63 ("drivers: net: cpsw: davinci_emac: move reading mac
    id to common file") did not only move the code for an am3517, it also
    added the slave parameter, resulting in an invalid (all zero) mac address
    being returned for an am3517, since it only has a single emac and the slave
    parameter is pointing to the second. So simply always read the first and
    valid mac-address.
    
    Cc: Mugunthan V N <mugunthanvnm@ti.com>
    Cc: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Jeroen Hofstee <jhofstee@victronenergy.com>

--- drivers/net/ethernet/ti/cpsw-common.c
+++ drivers/net/ethernet/ti/cpsw-common.c
@@ -74,8 +74,12 @@ int ti_cm_get_macid(struct device *dev, int slave, u8 *mac_addr)
 	if (of_machine_is_compatible("ti,am33xx"))
 		return cpsw_am33xx_cm_get_macid(dev, 0x630, slave, mac_addr);
 
+	/*
+	 * There is only one emac / mac address on an am3517 so ignore the
+	 * slave = 1 and always get the macid from slave 0.
+	 */
 	if (of_device_is_compatible(dev->of_node, "ti,am3517-emac"))
-		return davinci_emac_3517_get_macid(dev, 0x110, slave, mac_addr);
+		return davinci_emac_3517_get_macid(dev, 0x110, 0, mac_addr);
 
 	if (of_device_is_compatible(dev->of_node, "ti,dm816-emac"))
 		return cpsw_am33xx_cm_get_macid(dev, 0x30, slave, mac_addr);
