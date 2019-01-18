0c83cba299b9002f836d67a086550417b5546cfc
Jeroen Hofstee <jhofstee@victronenergy.com>
2019-01-18 11:01:21 +0100

    ubifs: trigger /proc/mounts poll in ubifs_ro_mode
    When ubi becomes read-only due to an io error, /proc/mounts is not signaled
    and select and friend don't get notified. This patch add support for that.
    
    Note: it doesn't respect namespaces!

--- fs/ubifs/io.c
+++ fs/ubifs/io.c
@@ -71,6 +71,7 @@
  */
 
 #include <linux/crc32.h>
+#include <linux/mount.h>
 #include <linux/slab.h>
 #include "ubifs.h"
 
@@ -87,6 +88,7 @@ void ubifs_ro_mode(struct ubifs_info *c, int err)
 		c->vfs_sb->s_flags |= SB_RDONLY;
 		ubifs_warn(c, "switched to read-only mode, error %d", err);
 		dump_stack();
+		mnt_options_changed();
 	}
 }
 
