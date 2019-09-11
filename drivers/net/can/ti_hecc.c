691054b41b803c2cd228b3a6de3b00d4103c13e7
Jeroen Hofstee <jhofstee@victronenergy.com>
2019-09-11 17:34:51 +0200

    can: ti_hecc: add missing state changes
    While the ti_hecc has interrupts to report when the error counters increase
    to a certain level and which change state it doesn't handle the case that
    the error counters go down again, so the reported state can actually be
    wrong. Since there is no interrupt for that, do update state based on the
    error counters, when the state is not error active and goes down again.
    
    Signed-off-by: Jeroen Hofstee <jhofstee@victronenergy.com>

--- drivers/net/can/ti_hecc.c
+++ drivers/net/can/ti_hecc.c
@@ -689,6 +689,23 @@ static irqreturn_t ti_hecc_interrupt(int irq, void *dev_id)
 			can_bus_off(ndev);
 			change_state(priv, rx_state, tx_state);
 		}
+	} else if (unlikely(priv->can.state != CAN_STATE_ERROR_ACTIVE)) {
+		enum can_state new_state, tx_state, rx_state;
+		u32 rec = hecc_read(priv, HECC_CANREC);
+		u32 tec = hecc_read(priv, HECC_CANTEC);
+
+		if (rec >= 128 || tec >= 128)
+			new_state = CAN_STATE_ERROR_PASSIVE;
+		else if (rec >= 96 || tec >= 96)
+			new_state = CAN_STATE_ERROR_WARNING;
+		else
+			new_state = CAN_STATE_ERROR_ACTIVE;
+
+		if (new_state < priv->can.state) {
+			rx_state = rec >= tec ? new_state : 0;
+			tx_state = rec <= tec ? new_state : 0;
+			change_state(priv, rx_state, tx_state);
+		}
 	}
 
 	if (int_status & HECC_CANGIF_GMIF) {
